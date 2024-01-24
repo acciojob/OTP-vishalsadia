8e1af6534d7e574c62bd6a945323195523bc1263
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2024-01-24 10:52:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 //your JS code here. If required.
+const codeContainer = document.querySelector(".code-container");
+const numberOfInputs = 8;
+let lastEnteredInputElement = null;
+
+function onInput(event) {
+    const currentElement = event.target;
+    console.log("event triggered");
+    if (currentElement.value) {
+        // for doing a click/focus just call the methods click() , and focus() on that HTML element object
+        // console.log("Next element", currentElement.nextElementSibling);
+        // console.log("Previous Element", currentElement.previousElementSibling);
+        lastEnteredInputElement = currentElement;
+        currentElement.nextElementSibling && currentElement.nextElementSibling.focus();
+    }
+}
+
+function onBackSpace(e) {
+    // i need to know last entered input element ?
+    // lastEnteredInputElement.value = "";
+    // if (lastEnteredInputElement.previousElementSibling) {
+    //     lastEnteredInputElement.previousElementSibling.focus();
+    //     lastEnteredInputElement = lastEnteredInputElement.previousElementSibling;
+    // }
+    // else {
+    //     lastEnteredInputElement = null;
+    // }
+    e.target.value = "";
+    e.target.previousElementSibling && e.target.previousElementSibling.focus();
+}
+
+function onEnter(e) {
+    if (e.target.value) {
+        e.target.nextElementSibling && e.target.nextElementSibling.focus();
+    }
+}
+
+function onKeyUp(e) {
+    if (e.key === "Enter") {
+        onEnter(e);
+    }
+    else if (e.key === "Backspace") {
+        onBackSpace(e);
+    }
+}
+
+for (let i = 1; i <= numberOfInputs; i++) {
+    const input = document.createElement("input");
+    input.className = "code";
+    input.maxLength = 1;
+
+    input.addEventListener("input", onInput);
+    input.addEventListener("keyup", onKeyUp);
+
+    codeContainer.appendChild(input);
+}
